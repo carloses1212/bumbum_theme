d5dffab5bb69990f8789eb7caa9b290da0f5da2d
carloses1212 <carloses121@gmail.com>
2023-06-28 12:14:45 -0500

    Add files via upload

---
+++ js/bumbum.js
@@ -0,0 +1,74 @@
+/* Load jQuery.
+--------------------------*/
+jQuery(document).ready(function ($) {
+  // Mobile menu.
+  $('.mobile-menu').click(function () {
+    $(this).next('.primary-menu-wrapper').toggleClass('active-menu');
+  });
+  $('.close-mobile-menu').click(function () {
+    $(this).closest('.primary-menu-wrapper').toggleClass('active-menu');
+  });
+  // Full page search.
+  $('.search-icon').on('click', function() {
+    $('.search-box').addClass('open');
+    return false;
+  });
+
+  $('.search-box-close').on('click', function() {
+    $('.search-box').removeClass('open');
+    return false;
+  });
+
+  // Scroll To Top.
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 80) {
+      $('.scrolltop').fadeIn('slow');
+    } else {
+      $('.scrolltop').fadeOut('slow');
+    }
+  });
+  $('.scrolltop').click(function () {
+    $('html, body').animate( { scrollTop: 0 }, 'slow');
+  });
+  
+  //Carrusel
+  var owl = $('.revive');
+  owl.owlCarousel({
+    margin: 10,
+    nav: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
+  //Banner
+  var owlb = $('.owl-carousel-banner');
+  owlb.owlCarousel({
+    margin: 10,
+    nav: false,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  })
+  
+  
+/* End document
+--------------------------*/
+});
